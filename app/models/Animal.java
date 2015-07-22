cc4b4626570c6267ce3568cade14723682cef45b
Richard Lewan <rlewan@sofi.org>
2015-07-22 13:44:14 -0600

    adding basic model of barn animal product

--- app/models/Animal.java
+++ app/models/Animal.java
@@ -2,12 +2,18 @@ package models;
 
 import play.data.validation.Constraints;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-@Entity
-public class Barn {
+//@Entity
+@Table(name = "animal")
+public class Animal {
 
     @Id
     @GeneratedValue
@@ -15,5 +21,8 @@ public class Barn {
 
     @Constraints.Required(message = "The name is required")
     public String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "animal")
+    public List<Product> products;
 
 }
