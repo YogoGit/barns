e7d171da6ee3009825b59f223f916db2e83676e6
rlsofi <rlewan@sofi.org>
2015-07-28 20:37:12 -0600

    corrections to auto-formatting. style guideline adherence for brackets.

--- node/barnAppraiser.js
+++ node/barnAppraiser.js
@@ -3,8 +3,9 @@ BarnAppraiser = function(barnsArray) {
   this.animals = [];
   // aggregate all of the barns' animals into one array.
   for (var i = 0; i < this.barns.length; i++) {
-    if (typeof this.barns[i].animals === "undefined")
+    if (typeof this.barns[i].animals === "undefined") {
       continue;
+    }
     this.animals = this.animals.concat(this.barns[i].animals);
   }
   this.totalAnimals = 0;
@@ -29,8 +30,9 @@ BarnAppraiser.prototype.calculateBarnsValue = function() {
 
 // The algorithm for calculating the value of the animals.
 BarnAppraiser.prototype.calculateAnimalsValue = function() {
-  if (this.animals.length < 1)
+  if (this.animals.length < 1) {
     return 0;
+  }
   var animalsValue = 0;
   var alpha = "..abcdefghijklmnopqrstuvwxyz";
   for (var j = 0; j < this.animals.length; j++) {
@@ -38,8 +40,9 @@ BarnAppraiser.prototype.calculateAnimalsValue = function() {
     this.totalAnimals += animalQuantity;
     var animalName = this.animals[j].name.toLowerCase();
     var alphaFactor = alpha.indexOf(animalName[0]);
-    if (alphaFactor <= 0)
+    if (alphaFactor <= 0) {
       alphaFactor = 1;
+    }
     animalsValue += alphaFactor * this.animalMultiple * animalQuantity;
   }
   return animalsValue;
